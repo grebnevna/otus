8f975c8cad5dfec832e2a8e50366bde46a2ac01b
nikitagrebnev <nikita.grebnev6@gmail.com>
2024-08-07 11:21:58 +0300

    otus_hw3

--- src/main/kotlin/ExceptionHandler.kt
+++ src/main/kotlin/ExceptionHandler.kt
@@ -6,38 +6,12 @@ class ExceptionHandler {
     companion object {
         private var store: MutableMap<String, MutableMap<String, (c: Command, e: Exception, q: Queue<Command>?) -> Command>> = mutableMapOf()
 
-        // 3
         fun handle(command: Command, exception: Exception, queue: Queue<Command>?): Command {
             val commandTypeName = command::class.java.typeName.split(".").last()
             val exceptionTypeName = exception::class.java.typeName.split(".").last()
 
             return (store[commandTypeName]!![exceptionTypeName]!!)(command, exception, queue)
         }
-
-//        // 5
-//        fun handle(command: Command, exception: Exception, queue: Queue<Command>): Command {
-//            val exceptionCommand = handle(command, exception)
-//
-//            return PutInQueueCommand(exceptionCommand, queue)
-//        }
-//
-//        // 6
-//        fun handleWithRetry(command: Command): Command? {
-//            var exceptionCommand: Command? = null
-//
-//            try {
-//                RetryCommand(command).execute()
-//            } catch (e: Exception) {
-//                exceptionCommand = handle(command, e)
-//            }
-//
-//            return exceptionCommand
-//        }
-//
-//        // 7
-//        fun handleWithRetry(command: Command, queue: Queue<Command>): Command {
-//            return PutInQueueCommand(HandleWithRetryCommand(command), queue)
-//        }
 
         fun registerHandler(
             commandTypeName: String,
